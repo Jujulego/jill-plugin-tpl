f3687c8a4d1023eaefb952d95ecc5427ffaceeb7
Julien Capellari <julien.capellari@gmail.com>
2022-12-29 12:19:29 +0100

    chore: setup build

---
+++ webpack.common.ts
@@ -0,0 +1,46 @@
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import { Configuration } from 'webpack';
+import path from 'node:path';
+
+// Config
+const commonConfig: Configuration = {
+  devtool: 'source-map',
+  target: 'node',
+  entry: './src/main',
+  output: {
+    clean: true,
+    path: path.resolve(__dirname, 'dist')
+  },
+  optimization: {
+    runtimeChunk: 'single',
+    moduleIds: 'deterministic',
+    splitChunks: {
+      chunks: 'all'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /[\\/]node_modules[\\/]/,
+        use: 'swc-loader',
+      },
+      {
+        test: /\.json$/,
+        type: 'json'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.jsx', '.ts', '.tsx'],
+  },
+  externals: [
+    'react-devtools-core',
+    'ws', // used only by ink for devtools
+  ],
+  plugins: [
+    new ForkTsCheckerWebpackPlugin(),
+  ]
+};
+
+export default commonConfig;
